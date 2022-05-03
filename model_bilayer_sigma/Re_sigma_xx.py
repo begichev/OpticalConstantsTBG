corrected normalization factor in Re sigma and automized calculations

model_bilayer_sigma/Re_sigma_xx.py
@@ -73,10 +73,10 @@ def Re_optical_conductivity(vals, vecs, omegas, gamma=0.002*t, e_win=0.05*t, kst
 
     def calc_sigma_one_point(omega):
         sigmas_mn = 0
-        kstepix = -1*kstep0
-        kstepiy = 0
-        kstepjx = 0.5*kstep0
-        kstepjy = -0.5*np.sqrt(3)*kstep0
+        kstepix = -0.5*np.sqrt(3)*kstep0*np.pi
+        kstepiy = 0.5*kstep0*2*np.pi
+        kstepjx = 0
+        kstepjy = -kstep0*2*np.pi
         for i in np.arange(steps**2):
                 energies = np.array(vals[i])
                 occup = np.array([2,2,0,0])
@@ -88,7 +88,7 @@ def Re_optical_conductivity(vals, vecs, omegas, gamma=0.002*t, e_win=0.05*t, kst
                 pairs = pick_up_transition_pairs(energies, omega, e_win, occup)   
                 if len(pairs):
                     sigmas_mn += np.sum([calc_sigma_mn_pair(i, int(pair[0]), int(pair[1]), omega, occup, v) for pair in pairs])
-        return 6*np.sqrt(3)*kstep0**2/np.pi*np.sum(sigmas_mn)
+        return 4*np.sqrt(3)*kstep0**2*np.sum(sigmas_mn)
     
     sigmas = [calc_sigma_one_point(omega) for omega in omegas]
     return np.array(sigmas)
@@ -106,9 +106,9 @@ def dimensionless_v(kx, ky):
     block2 = np.conj(block)
     return block_diag(block, block2)
 
-def calc_optical_conductivity(omegas=np.arange(0.05, 1.5, 0.05), gamma=0.002*t, e_win=0.02*t, save_to='ab_sigma.txt'):
-    kstep0 = 0.001
-    gamma = 0.02
+def calc_optical_conductivity(omegas=np.arange(0.05, 1.5, 0.05), gamma=0.002*t, e_win=0.02*t, save_to='ab_sigma.txt',kstep0=0.001):
+ #   kstep0 = 0.0005
+    gamma = 0.04
     steps = int(2/3/kstep0)
     energies_load_from='ab_bilayer_energies_'+str(kstep0)+'.txt'
     wavefunctions_load_from='ab_bilayer_wavefunctions_'+str(kstep0)+'.txt'

model_bilayer_sigma/bilayer_spectrum.py
@@ -17,32 +17,36 @@ def Hamiltonian(kx, ky):
 	block1[0][1] = -t*S
 	block2 = np.conj(block1)
 	ham = block_diag(block1, block2)
+	"""
 	sigma_x = np.matrix([[0,1],[1,0]])
 	off_diag = np.matrix([[t_layers,0],[0,0]])
 	ham += np.kron(sigma_x, off_diag)
+	"""
+	ham[2][0]=ham[0][2]=t_layers
 	return ham
 
-a = 2.46 # angstrom
-k = 2*np.pi/a # inverse angstrom
-Kx = k/np.sqrt(3)
-Ky = k/3 # Dirac point coordinates
-kstep0 = 0.001
-steps = int(2/3/kstep0) # number of points in brilloine zone near K. whole BZ is covered with 3*steps
-kstepix = -1*kstep0*k
-kstepiy = 0
-kstepjx = 0.5*kstep0*k
-kstepjy = -0.5*np.sqrt(3)*kstep0*k
-eigenenergies = []
-eigenfunctions = []
-for i in np.arange(steps): 
-	for j in np.arange(steps):
-		ham_mono = Hamiltonian(Kx+i*kstepix+j*kstepjx, Ky+i*kstepiy+j*kstepjy)
-		eigenvalues, eigenvectors = np.linalg.eigh(ham_mono)
-		eigenenergies.append(eigenvalues)
-		eigenfunctions.extend(eigenvectors)
-real_eigenenergies = np.real(eigenenergies)
-energies_save_to='ab_bilayer_energies_'+str(N)+'_'+str(kstep0)+'.txt'
-wavefunctions_save_to='ab_bilayer_wavefunctions_'+str(N)+'_'+str(kstep0)+'.txt'
+def CalcSpectra(kstep0=0.001):
+	a = 2.46 # angstrom
+	k = 2*np.pi/a # inverse angstrom
+	Kx = k/np.sqrt(3)
+	Ky = k/3 # Dirac point coordinates
+#	kstep0 = 0.0005
+	steps = int(2/3/kstep0) # number of points in brilloine zone near K. whole BZ is covered with 3*steps
+	kstepix = -0.5*np.sqrt(3)*kstep0*k
+	kstepiy = 0.5*kstep0*k
+	kstepjx = 0
+	kstepjy = -kstep0*k
+	eigenenergies = []
+	eigenfunctions = []
+	for i in np.arange(steps): 
+		for j in np.arange(steps):
+			ham_mono = Hamiltonian(Kx+i*kstepix+j*kstepjx, Ky+i*kstepiy+j*kstepjy)
+			eigenvalues, eigenvectors = np.linalg.eigh(ham_mono)
+			eigenenergies.append(eigenvalues)
+			eigenfunctions.extend(eigenvectors)
+	real_eigenenergies = np.real(eigenenergies)
+	energies_save_to='ab_bilayer_energies_'+str(kstep0)+'.txt'
+	wavefunctions_save_to='ab_bilayer_wavefunctions_'+str(kstep0)+'.txt'
 
-np.savetxt(energies_save_to, real_eigenenergies)
-np.savetxt(wavefunctions_save_to,eigenfunctions)
+	np.savetxt(energies_save_to, real_eigenenergies)
+	np.savetxt(wavefunctions_save_to,eigenfunctions)

model_bilayer_sigma/readfiles.py
@@ -1,35 +1,14 @@
 import numpy as np
-from Re_sigma_xx import calc_optical_conductivity, occup_0K, pick_up_transition_pairs
+from Re_sigma_xx import calc_optical_conductivity, plot_optical_cond
+from bilayer_spectrum import CalcSpectra
 
-#eigenenergies = np.loadtxt("monolayer_energies.txt", dtype=float)
-#eigenfunctions = np.loadtxt("monolayer_wavefunctions.txt", dtype=complex)
+kstep0 = 0.001
+omegas=np.arange(0.05, 1.5, 0.05)
+gamma = 0.04 # in units of t_layers
+t_layers = 0.4 # eV hopping between A and B atoms in AB bilayer
+CalcSpectra(kstep0)
+calc_optical_conductivity(omegas=omegas, gamma=gamma*t_layers,kstep0=kstep0)
+filename = 'ab_bilayer_sigma'+str(kstep0)+'_gamma'+str(gamma)+'.txt'
+plot_save_to = 'ab_bilayer_sigma'+str(kstep0)+'_gamma'+str(gamma)+'.pdf'
 
-#print(np.size(eigenenergies), '\t', np.size(eigenfunctions))
-
-#calc_optical_conductivity()
-
-#monolayer_occup_save_to='monolayer_occup.txt'
-#np.savetxt(monolayer_occup_save_to, occup_0K(eigenenergies))
-
-#transition_pairs_save_to = 'monolayer_transition_pairs0.5eV.txt'
-#np.savetxt(transition_pairs_save_to, pick_up_transition_pairs(eigenenergies,0.5, 0.3, occup_0K(eigenenergies)))
-
-#transition_pairs = np.loadtxt('monolayer_transition_pairs0.5eV.txt')
-#print(eigenenergies[int(transition_pairs[0][0])], '\n')
-#print(eigenenergies[int(transition_pairs[0][1])])
-
-#print(transition_pairs[0])
-
-calc_optical_conductivity()
-#vals = np.loadtxt("monolayer_energies_20_0.005_lined.txt",dtype=float)
-#occup = occup_0K(vals[0])
-#i = 1000
-#print(vals[i])
-#print(pick_up_transition_pairs(vals[i],0.5,0.05,occup))
-
-
-#vecs = np.loadtxt("monolayer_wavefunctions_20_0.005_lined.txt",dtype=complex)
-#vals = np.loadtxt("monolayer_energies_20_0.005_lined.txt",dtype=float)
-#print(vals[len(vals)])
-
-#print(eigenfunctions[0])
+plot_optical_cond(filename, plot_save_to)