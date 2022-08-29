e46fe0d0b1ca11923d1006143f50fe3564435c8e
begichev <begichev.iv@phystech.edu>
2022-08-29 15:38:45 +0300

    deleted extra stuff

--- plotResigmaxx.gp
+++
@@ -1,6 +0,0 @@
-set pm3d map
-set xrange [10:19]
-set xlabel "Angle (deg.)"
-set yrange [1300:2000]
-set ylabel "Energy (meV)"
-splot 'sigmacopy.dat' u 1:2:3
--- real_ab_bilayer_sigma/plotter.py
+++
@@ -1,8 +0,0 @@
-import matplotlib as plt
-import numpy as np
-from Re_sigma_xx import plot_optical_cond
-
-filename = 'bilayer_sigma_10_0.02_gamma0.002_ewin0.02n21_changev.txt'
-plot_save_to = 'bilayer_sigma_10_0.02_gamma0.002_ewin0.02n21_changev_scatter.pdf'
-
-plot_optical_cond(filename, plot_save_to)
